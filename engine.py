e52c2c0fa8a1d18f66237738dc12382b7fdd0804
duyquang <quang.doduy99@gmail.com>
2021-06-17 09:34:09 +0700

    update

---
+++ engine.py
@@ -0,0 +1,18 @@
+from constants import *
+import numpy as np
+
+class GameEngine:
+    def __init__(self) -> None:
+        pass
+
+    # HE TOA DO DESCARTES TRONG KHONG GIAN 2 CHIEU
+    def descartes_coordinates():
+        x_0 = 0.0
+        y_0 = 0.0
+        alpha = 90
+        
+        pass
+
+    # DRAW ON FRAME WITH X,Y IS CENTER OF SCREEN
+    def draw():
+        pass
