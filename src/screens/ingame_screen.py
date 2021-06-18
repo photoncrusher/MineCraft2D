83dc472839c8d9a2398bf946c694983c3dd9d97c
Nguyễn Huy Hoàng <58360612+dranhclub@users.noreply.github.com>
2021-06-18 15:30:32 +0700

    delete user_interface.py

--- src/screens/ingame_screen.py
+++ src/screens/ingame_screen.py
@@ -4,8 +4,6 @@ import sys
 import time
 from .screen import Screen
 import src.constants as const
-from src.screens.user_interfaces import *
-import pygame_gui
 from src.constants import *
 from engine import *
 
@@ -19,9 +17,9 @@ class InGameScreen(Screen):
 
     def show(self):
         game = GameEngine(self.main.seed)
-        manager = pygame_gui.UIManager((self.width, self.heigth))
-        ui = UserInterface(manager, self)
-        ui.create_ui_01(manager, 'PAUSE_GAME_UI')
+        # manager = pygame_gui.UIManager((self.width, self.heigth))
+        # ui = UserInterface(manager, self)
+        # ui.create_ui_01(manager, 'PAUSE_GAME_UI')
         window = self.main.window
         myfont = pygame.font.Font(const.FONT, 30)
         text2 = myfont.render(f'Seed: {self.main.seed}', True, (255, 0, 0))
@@ -34,13 +32,13 @@ class InGameScreen(Screen):
                     pygame.quit()
                     sys.exit()
                 game.get_event(event)
-                manager.process_events(event)
-            manager.update(time_delta)
+                # manager.process_events(event)
+            # manager.update(time_delta)
             window.fill((255, 255, 255))
             window.blit(self.day_sky,(0,0))
 
-            if pause:
-                ui.draw(window, manager)
+            # if pause:
+            #     ui.draw(window, manager)
             x_y = game.draw(window)
             text1 = myfont.render('x: ' + str(round(x_y[0]/20, 2)) + ', y: ' + str(round(x_y[1]/20, 2)), True, (0, 0, 0))
             text3 = myfont.render('stat: '+str(x_y[3]), True, (255, 0, 0))
